3419535954b0be4be768e13f5559b3456494a253
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-05-05 19:41:55 -0400

    chore: configure build and serve scripts

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle-prod.js",
+  },
+  resolve: {
+    extensions: ["tsx", ".js", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    port: 8000,
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    historyApiFallback: true,
+    writeToDisk: true,
+    open: true,
+  },
+};
